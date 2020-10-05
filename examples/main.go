2c5158987f9ef7af0b6a5f52955090428bbf034d
Daniel <daniel.unverricht@pentlandfirth.com>
2020-10-05 11:12:39 +0200

    add test for file-download

--- examples/main.go
+++ examples/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/dunv/uhttp"
 	"github.com/dunv/ulog"
@@ -20,6 +22,20 @@ func main() {
 		writer.WriteHeader(http.StatusAccepted)
 
 		ulog.LogIfErrorSecondArg(writer.Write([]byte(`{"nothing":"toSay"}` + "\n")))
+		return nil
+	})))
+
+	u.Handle("/testDatei", uhttp.NewHandler(uhttp.WithGet(func(r *http.Request, ret *int) interface{} {
+		writer := r.Context().Value(uhttp.CtxKeyResponseWriter).(http.ResponseWriter)
+		writer.WriteHeader(http.StatusAccepted)
+
+		f, err := os.OpenFile("./testDatei", os.O_RDONLY, 0644)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		ulog.LogIfErrorSecondArg(io.Copy(writer, f))
 		return nil
 	})))
 
