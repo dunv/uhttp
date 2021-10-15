3ef20da614296d3e74e0e9fefe79332a4faa1ef4
Daniel <daniel.unverricht@pentlandfirth.com>
2021-10-15 16:05:57 +0200

    add test for client-helper for error

---
+++ HttpResponseErrorModel_test.go
@@ -0,0 +1,32 @@
+package uhttp
+
+import (
+	"bufio"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/dunv/ulog"
+	"github.com/stretchr/testify/require"
+)
+
+func TestErrorModel(t *testing.T) {
+	ulog.SetWriter(bufio.NewWriter(nil), nil)
+	u := NewUHTTP()
+
+	handler := NewHandler(
+		WithGet(func(r *http.Request, ret *int) interface{} {
+			return errors.New("err from handler")
+		}),
+	)
+	u.Handle("/test", handler)
+	req := httptest.NewRequest("GET", "/test", nil)
+	w := httptest.NewRecorder()
+	u.ServeMux().ServeHTTP(w, req)
+	res := w.Result()
+	require.Equal(t, http.StatusBadRequest, res.StatusCode)
+	parsedErr, err := ErrorFromHttpResponseBody(res.Body)
+	require.NoError(t, err)
+	require.Equal(t, "err from handler", parsedErr.Error())
+}
